a66ded609f11675c6ff786c6cda1dd206c4c72a4
Saliinger <alexisnoukan@gmail.com>
2026-04-03 14:21:11 +0200

    add: frontend docker file + mariadb setup

---
+++ home/mariadb/tools/run.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+if [ ! -d /var/lib/mysql/mysql ]; then
+	mysql_install_db
+	mariadbd &
+	while ! mariadb-admin ping --silent; do
+		sleep 1
+	done
+	mariadb -e "CREATE DATABASE ${DB_NAME};"
+	mariadb -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PWD}';"
+	mariadb -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
+	mariadb-admin shutdown
+fi
+
+exec mariadbd
